c77e70cf3362dcf3c60e171d7c367f98a2dace90
Meerim <44379294+kgleader@users.noreply.github.com>
2023-01-07 02:01:08 +0600

    first commit of BLOC

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:bloc01/example%201/home_page1.dart';
+import 'package:bloc01/example%202/home_page2.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp2());
+}
